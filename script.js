b913b57606567955f196fd1e6ee9df0eb277f05d
Captain <71038018+karimi65@users.noreply.github.com>
2020-12-08 09:52:00 -0600

    listener for save-button

--- script.js
+++ script.js
@@ -3,5 +3,30 @@ $(document).ready(function() {
 // Display the current date and time
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'))
 
+// add click listener for save-button
+$('saveBtn').on('click', saveMySchedule())
+
+function saveMySchedule() {
+    // console.log(this);
+// get values
+    var text = $(this).siblings('.describtion').val();
+    var time = $(this).parent().attr('id');
+// put data in local storage
+    localStorage.setItem(time, text);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
