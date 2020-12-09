e114049b56ecf47018e1b57d70f4e2700131d058
Captain <71038018+karimi65@users.noreply.github.com>
2020-12-09 08:05:18 -0600

    loop over time blocks

--- script.js
+++ script.js
@@ -1,42 +1,57 @@
 $(document).ready(function() {
 
-// Display the current date and time
-$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'))
+    // Display the current date and time
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'))
 
-// add click listener for save-button
-$('saveBtn').on('click', saveMySchedule())
+    // add click listener for save-button
+    $('saveBtn').on('click', saveMySchedule())
 
-function saveMySchedule() {
-    // console.log(this);
-// get values
-    var text = $(this).siblings('.describtion').val();
-    var time = $(this).parent().attr('id');
-// put data in local storage
-    localStorage.setItem(time, text);
-}
+    function saveMySchedule() {
+        // console.log(this);
+        // get values
+        var text = $(this).siblings('.describtion').val();
+        var time = $(this).parent().attr('id');
+        // put data in local storage
+        localStorage.setItem(time, text);
+    }
 
-// Get data from local storage
-$('#t9am .description').val(localStorage.getItem('t9am'));
-$('#t10am .description').val(localStorage.getItem('t10am'));
-$('#t11am .description').val(localStorage.getItem('t11am'));
-$('#t12pm .description').val(localStorage.getItem('t12pm'));
-$('#t1pm .description').val(localStorage.getItem('t1pm'));
-$('#t2pm .description').val(localStorage.getItem('t2pm'));
-$('#t3pm .description').val(localStorage.getItem('t3pm'));
-$('#t4pm .description').val(localStorage.getItem('t4pm'));
-$('#t5pm .description').val(localStorage.getItem('t5pm'));
-
-
+    // Get data from local storage
+    $('#t9am .description').val(localStorage.getItem('t9am'));
+    $('#t10am .description').val(localStorage.getItem('t10am'));
+    $('#t11am .description').val(localStorage.getItem('t11am'));
+    $('#t12pm .description').val(localStorage.getItem('t12pm'));
+    $('#t1pm .description').val(localStorage.getItem('t1pm'));
+    $('#t2pm .description').val(localStorage.getItem('t2pm'));
+    $('#t3pm .description').val(localStorage.getItem('t3pm'));
+    $('#t4pm .description').val(localStorage.getItem('t4pm'));
+    $('#t5pm .description').val(localStorage.getItem('t5pm'));
 
 
+    function timeTracker() {
+        var thisTime = moment().hour();
 
+        // loop for time blocks
+        $('.time-block').each(function () {
+            var blockHour = parseInt($(this).attr('id').split('hour')[1]);
+            console.log (blockHour, thisTime);
 
-
-
-
-
-
-
-
-
+            // if moved past this time
+            if (blockHour < thisTime) {
+                $(this).addClass('past');
+                $(this).removeClass('future');
+                $(this).removeClass('present');
+            } 
+            else if (blockHour === thisTime) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+                $(this).removeClass('future');  
+            }
+            else {
+                $(this).removeClass('present');
+                $(this).removeClass('past');
+                $(this).addClass('future');  
+            }
+        })
+    }
+    timeTracker();
 })
